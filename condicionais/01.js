3023a973c4dcb417528fcf77999d5dee3fe597b8
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-09-10 11:06:30 -0300

    condicionais: exercicio 01 resolvido

--- condicionais/01.js
+++ condicionais/01.js
@@ -1,73 +1,74 @@
-let a = 1
-let b = 2
-let c = 3
+const a = 1
+const b = 2
+const c = 3
 
-// Resultado:
-if (6 < 2 * 5) {
+// Resultado: "Hello, world!"
+if (6 < 2 * 5) { // 6 < 10
 	console.log("Hello, world!")
 }
 
-// Resultado:
-if (a > b && a > c) {
+// Resultado: 2222
+if (a > b && a > c) { // 1 > 2 && ..
 	console.log(1111)
 } else {
 	console.log(2222)
 }
 
-// Resultado:
-if (a < c) {
+// Resultado: "*"
+if (a < c) { // 1 < 3
 	console.log("*")
-} else if (a === b) {
+} else if (a === b) { // não executa
 	console.log("&")
-} else {
+} else { // não executa
 	console.log("$")
 }
 
-// Resultado:
-if (a < b) {
+// Resultado: "####"
+if (a < b) { // 1 < 2
 	console.log("####")
-} else {
+} else { // não executa
 	console.log("&&&&")
 }
 
-// Resultado:
+// Resultado: 100 200 0
 const x = 100
 const y = 200
-if (x > 100 && y <= 200) {
+if (y <= 200 && x > 100) { // 200 <= 200 && 100 > 100
 	console.log(`${x} ${y} ${x + y}`)
-} else {
+} else { // não executa
 	console.log(`${x} ${y} ${2 * x - y}`)
 }
 
-// Resultado:
-if (a < c) {
+// Resultado: "*"
+if (a < c) { // 1 < 3
 	console.log("*")
-} else if (a === c) {
+} else if (a === c) { // não executa
 	console.log("&")
-} else {
+} else { // não executa
 	console.log("$")
 }
 
-// Resultado:
-if (a++ > b++ || a-- > 0) {
+// a++ -> a = a + 1 
+// Resultado: 1 3 4
+if (a++ > b++ || a-- > 0) { // 2 > 3 || 1 > 0
 	c++
-} else {
+} else { // não executa
 	c--
 }
 console.log(`${a} ${b} ${c}`)
 
-// Resultado:
-if (a < b) {
+// Resultado: "####"
+if (a < b) { // 1 < 3
 	console.log("####")
-} else {
+} else { // não executa
 	console.log("&&&&")
 }
 
-// Resultado:
-if (a < c) {
+// Resultado: "****"
+if (a < c) { // 1 < 4
 	console.log("****")
-} else if (b > a) {
+} else if (b > a) { // não executa
 	console.log("&&&&")
-} else {
+} else { // não executa
 	console.log("####")
 }
