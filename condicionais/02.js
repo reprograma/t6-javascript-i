4ca2662a4001532e71a8e9859d7c5b731877522e
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-09-10 19:15:14 -0300

    condicionais: exercicios 02 e 03 resolvidos

--- condicionais/02.js
+++ condicionais/02.js
@@ -2,9 +2,9 @@
 // Javascript que atribui o
 // valor `1` a `x` se `x`
 // for maior do que `y`.
-
-
-
+if (x > y) {
+	x = 1
+}
 
 // Escreva a expressão em
 // Javascript que aumenta o
@@ -12,33 +12,49 @@
 // em 5 unidades caso o valor
 // inicial de `score` esteja
 // entre 80 e 90.
-
-
-
+if (score >= 80 && score <= 90) {
+	score += 5 // score = score + 5
+}
 
 // Reescreva a seguinte condicional
 // sem usar o operador `!`:
 // item = ! ( i < 10 || v >= 50 )
-
-
-
+item = ( i < 10 || v >= 50 ) === false
+// ou...
+item = ( i >= 10 && v < 50 )
+// refs: 
+// - http://www.math.toronto.edu/preparing-for-calculus/3_logic/we_3_negation.html
+// - https://centraldefavoritos.com.br/2017/01/02/negacao-de-proposicoes/ (ver: "Negação de uma Proposição Disjuntiva")
 
 // Escreva a expressão em
 // Javascript que retorna `true`
 // se um número é par e `false`
 // se um número é impar.
-
-
-
+if (n % 2 === 0) {
+	console.log(true)
+} else {
+	console.log(false)
+}
+// ou ...
+if (n & 1 === 0) {
+	console.log(true)
+} else {
+	console.log(false)
+}
+// refs: 
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
+// - https://code.tutsplus.com/articles/understanding-bitwise-operators--active-11301 (ver: "The & Operator")
 
 // Escreva a expressão em
 // Javascript que retorna `true` 
 // se dois números forem positivos
 // e `false` se dois números 
 // forem negativos.
-
-
-
+if (x > 0 && y > 0) {
+	console.log(true)
+} else if(x < 0 && y < 0) {
+	console.log(false)
+}
 
 // Escreva a expressão em
 // Javascript que retorna `true`
@@ -46,3 +62,28 @@
 // sinal (+ / -) e `false` 
 // se dois números tiverem sinais
 // diferentes.
+if (x > 0 && y > 0 || 
+	x < 0 && y < 0) {
+	console.log(true)
+} else {
+	console.log(false)
+}
+
+// ou...
+
+if (x > 0 && y > 0) {
+	console.log(true)
+} else if (x < 0 && y < 0) {
+	console.log(true)
+} else {
+	console.log(false)
+}
+
+// ou...
+
+if (Math.sign(x) === Math.sign(y) && 
+	Math.sign(x) !== 0) {
+	console.log(true)
+} else {
+	console.log(false)
+}
--- condicionais/03.js
+++ condicionais/03.js
@@ -2,6 +2,27 @@
 // quando, dado um mesmo input, devolvem
 // o mesmo output. Qual das próximas
 // condicionais são equivalentes? Porque?
+
+// Resposta: A == C, o B é diferente.
+
+// Porque...
+
+// rand = 1
+// A -> O valor é positivo: 1
+// B -> O valor é positivo: 1 && O valor é zero!
+// C -> O valor é positivo: 1
+
+// rand = 0
+// A -> O valor é zero!
+// B -> O valor é zero!
+// C -> O valor é zero!
+
+// rand = -1
+// A -> O valor é negativo: -1!
+// B -> O valor é negativo: -1!
+// C -> O valor é negativo: -1!
+
+// Gera um numero randomico entre -10 e 10.
 const rand = Math.floor(Math.random() * 21) - 10
 
 // A
@@ -35,3 +56,13 @@ if (rand < 0) {
 if (rand === 0) {
 	console.log(`O valor é zero!`)
 }
+
+// Outra forma de escrever, equivalente ao A e B...
+//
+// if (rand > 0) {
+// 	console.log(`O valor é positivo: ${rand}!`)
+// } else if (rand < 0) {
+// 	console.log(`O valor é negativo: ${rand}!`)
+// } else {
+// 	console.log(`O valor é zero!`)
+// }
