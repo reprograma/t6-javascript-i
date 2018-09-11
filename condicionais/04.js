02ccf22a641cfc50ad9415a9c62ebd43b129164c
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-09-11 10:17:47 -0300

    condicionais: exercicio 04 resolvido

--- condicionais/04.js
+++ condicionais/04.js
@@ -25,9 +25,23 @@
 // 	default:
 // 		throw "Dia inválido!"
 // }
-
-
-
+if (day === 0) {
+	console.log("Segunda")
+} else if (day === 1) {
+	console.log("Terça")
+} else if (day === 2) {
+	console.log("Quarta")
+} else if (day === 3) {
+	console.log("Quinta")
+} else if (day === 4) {
+	console.log("Sexta")
+} else if (day === 5) {
+	console.log("Sábado")
+} else if (day === 6) {
+	console.log("Domingo")
+} else {
+	alert("Dia inválido!")
+}
 
 // Reescreva a próxima expressão usando
 // o operador ternário
@@ -37,15 +51,8 @@
 // } else {
 // 	rand = false
 // }
+let rand = Math.random() > 0.5 ? true : false
 
-
-
-
-
-// const r = Math.random() * 256
-// const g = Math.random() * 256
-// const b = Math.random() * 256
-//
 // Reescreva a próxima expressão usando
 // o operador ternário
 // let color
@@ -56,3 +63,11 @@
 // } else if (b > r && b > g) {
 // 	color = "Azul"
 // }
+const r = Math.random() * 256
+const g = Math.random() * 256
+const b = Math.random() * 256
+let color = r > g && r > b ? "Vermelho"
+	: g > b && g > r ? "Verde" 
+	: b > r && b > g ? "Azul"
+	: "Cor indefinida"
+
