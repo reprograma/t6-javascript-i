51ba5af4ef7fda4874f6627dd81eb52805c774df
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-09-14 16:00:23 -0300

    objetos: exercicio 01 resolvido

--- objetos/01.js
+++ objetos/01.js
@@ -4,34 +4,41 @@
 // um dos pokemons da lista
 // de pokemons do Wikipedia.
 // ref: https://pt.wikipedia.org/wiki/Lista_de_Pok%C3%A9mon
-
-
-
+const pokemon = {
+	numero_nacional: 143,
+	nome: "Snorlax",
+	nome_japones: "Kabigon",
+	numero_johnto: 230,
+	evolui_de: "Munchlax",
+	janiarli_tem: true
+}
 
 // 2.
 // Printe no console a seguinte
 // frase: "Hello, my name is < >.
 // And my name in Japanese is < >."
-
-
-
+pokemon.diga_ola = function () {
+	console.log(`Hello, my name is ${this.nome}. And my name in Japanese is ${this.nome_japones}.`)
+}
 
 // 3.
 // Adicione uma nova propriedade ao
 // seu pokemon chamada `can_fly` 
 // (ou algo do tipo) e defina
 // ela como `true` ou `false`.
-
-
-
+pokemon.can_fly = false
 
 // 4.
 // Adicione um método ao seu pokemon
 // chamado `fly` que retorna "Sorry,
 // I can't fly" ou "Flyyyyiinnngggg!".
-
-
-
+pokemon.fly = function () {
+	if (this.can_fly) {
+		return "Flyyyyiinnngggg!"
+	} else {
+		return "Sorry, I can't fly"
+	}
+}
 
 // 5.
 // Adicione um método chamado
